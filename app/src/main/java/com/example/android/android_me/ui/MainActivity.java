819a6dfa8a0193c0437752d67f85d72fb3044882
bao.do <bao.do@innovapost.com>
2019-03-13 21:50:45 -0400

    inter fragment commmunication

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -16,15 +16,24 @@
 
 package com.example.android.android_me.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
 
 // This activity is responsible for displaying the master list of all images
 // Implement the MasterListFragment callback, OnImageClickListener
 public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener{
+
+
+    private int headIndex;
+    private int bodyIndex;
+    private int legIndex;
 
 
     @Override
@@ -39,12 +48,44 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
         // Create a Toast that displays the position that was clicked
         Toast.makeText(this, "Position clicked = " + position, Toast.LENGTH_SHORT).show();
 
+        int bodyPartNumber =  position/12;
+        int listIndex = position - 12*bodyPartNumber;
+
+        switch (bodyPartNumber){
+            case 0: {
+                headIndex = listIndex;
+                break;
+            }
+            case 1: {
+                bodyIndex = listIndex;
+                break;
+            }
+            case 2: {
+                legIndex = listIndex;
+                break;
+            }
+            default:
+                break;
+        }
+        Bundle bundle = new Bundle();
+        bundle.putInt("headIndex", headIndex);
+        bundle.putInt("bodyIndex", bodyIndex);
+        bundle.putInt("legIndex", legIndex);
+
+        final Intent androidMe = new Intent(this, AndroidMeActivity.class);
+        androidMe.putExtras(bundle);
         // TODO (2) Based on where a user has clicked, store the selected list index for the head, body, and leg BodyPartFragments
 
         // TODO (3) Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
 
         // TODO (4) Get a reference to the "Next" button and launch the intent when this button is clicked
-
+        Button nextButton = (Button) findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(androidMe);
+            }
+        });
     }
 
 }
